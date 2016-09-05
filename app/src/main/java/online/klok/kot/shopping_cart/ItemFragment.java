6deb552e70903b4995c079f24b8642101afe2788
sooorajjj <sooorajjj@gmail.com>
2016-09-05 21:52:30 +0530

    phew ! finally
    base for my shopping cart

--- app/src/main/java/online/klok/kot/shopping_cart/ItemFragment.java
+++ app/src/main/java/online/klok/kot/shopping_cart/ItemFragment.java
@@ -1,30 +1,76 @@
 package online.klok.kot.shopping_cart;
-
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import online.klok.kot.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ItemFragment extends Fragment {
+public class ItemFragment extends Fragment implements ShoppingCartAdapter.OnShoppingCartItemClicked {
 
+    private static final String LOG_TAG = ItemFragment.class.getSimpleName();
 
-    public ItemFragment() {
-        // Required empty public constructor
-    }
-
+    private RecyclerView rvShoppingCart;
+    private TextView tvTotalItems, tvTotalPrice;
+    private int currentCartCount, currentTotalPrice;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_item, container, false);
+        View view = inflater.inflate(R.layout.fragment_item, container, false);
+
+        tvTotalItems = (TextView) view.findViewById(R.id.tv_total_items);
+        tvTotalPrice = (TextView) view.findViewById(R.id.tv_total_price);
+
+
+        tvTotalItems.setText("Total Item : " + currentCartCount);
+        tvTotalPrice.setText("Total Price : " + currentTotalPrice);
+
+        rvShoppingCart = (RecyclerView) view.findViewById(R.id.rv_shopping_cart);
+        rvShoppingCart.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
+        rvShoppingCart.setAdapter(new ShoppingCartAdapter(this, getShoppingCartItems()));
+
+        return view;
+    }
+
+    private ArrayList<ShoppingCartPOJO> getShoppingCartItems() {
+        ArrayList<ShoppingCartPOJO> itemList = new ArrayList<>();
+
+        for (int i = 0; i < 20; i++) {
+            ShoppingCartPOJO shoppingCartPOJO = new ShoppingCartPOJO();
+            shoppingCartPOJO.setName("Item " + (i + 1));
+            shoppingCartPOJO.setPrice(i + 1);
+            itemList.add(shoppingCartPOJO);
+        }
+
+        Log.e(LOG_TAG, "Total items size :" + itemList.size());
+
+        return itemList;
+    }
+
+    @Override
+    public void onShoppingCartAddClicked(int price) {
+
+
+        currentCartCount = currentCartCount + 1;
+        currentTotalPrice = currentTotalPrice + price;
+
+
+        tvTotalItems.setText("Total Item : " + currentCartCount);
+        tvTotalPrice.setText("Total Price : " + currentTotalPrice);
+
+
     }
 
 }
